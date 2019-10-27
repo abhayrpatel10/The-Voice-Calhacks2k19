c1180cb3d7ed000654114fe7ff2fb781d08e9b53
abhayrpatel10 <abhayrpatel10@gmail.com>
2019-10-27 17:48:48 +0530

    deploying

--- app.py
+++ app.py
@@ -11,12 +11,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Landing.html')
+    return render_template('index.html')
 
 @app.route('/recvid')
 def index():
     return render_template('index.html')
 
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/index.html')
+def ind():
+    return render_template('index.html')
+
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+@app.route('/docu.html')
+def docu():
+    return render_template('docu.html')
 def gen(camera):
     while True:
         frame = camera.get_frame()
@@ -29,9 +44,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/login')
-def login():
-    return "login"
+
 
 @app.route('/docusign')
 def docsign():
